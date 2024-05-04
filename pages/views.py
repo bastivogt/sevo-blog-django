f7cc724368f15129f568f2aa5f8d6801467d84e4
Sevo <betont.online@googlemail.com>
2024-05-04 15:27:19 +0200

    some changes

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from . import models
+from . import helper
 
 # Create your views here.
 
 
 def page_single(request, slug):
+    pages_main = helper.get_main_pages()
+    pages_meta = helper.get_meta_pages()
+    page = get_object_or_404(models.Page, slug=slug)
     return render(request, "pages/page_single.html", {
-        "title": "Page single",
-        "slug": slug
+        "page": page, 
+        "pages_main": pages_main,
+        "pages_meta": pages_meta
     })
