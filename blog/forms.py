3d5ffc1969a08874c45020a6febaa54fea64b77f
Sevo <betont.online@googlemail.com>
2024-05-07 22:39:11 +0200

    bootstrapping the comment-form

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 
 class CommentForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=100)
-    email = forms.EmailField(label="E-Mail")
-    comment = forms.CharField(label="Kommentar", widget=forms.Textarea)
+    name = forms.CharField(label="Name", label_suffix="", max_length=100)
+    email = forms.EmailField(label="E-Mail", label_suffix="")
+    comment = forms.CharField(label="Kommentar", label_suffix="", widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs["class"] = "form-control"
+        self.fields["email"].widget.attrs["class"] = "form-control"
+        self.fields["comment"].widget.attrs["class"] = "form-control"
